408579d520836e21c0b41cd7cb45fbdc09af0e19
Luke Kelly <lkelly93@gmail.com>
2020-08-23 16:03:46 -0700

    More tests

--- program/program.go
+++ program/program.go
@@ -30,7 +30,8 @@ func NewProgram(lang string, code string) (*Program, error) {
 }
 
 //Run runs the given program and then returns the output from that given program
-//If the program errors Run only returns the error messages
+//Run returns the result of the run and the err message. If err == nil then the
+//run was successful
 func Run(prog *Program) (string, error) {
 	runnerFileFunctor := GetFunctor(prog.Lang)
 
--- program/program_test.go
+++ program/program_test.go
@@ -55,8 +55,35 @@ func TestRunPythonCode(t *testing.T) {
 	assertEquals(expected, actual, t)
 }
 
+func TestRunBadPythonCode(t *testing.T) {
+	prog, _ := program.NewProgram("python", "print('Hi")
+	actual, err := program.Run(prog)
+	expected := "  File \"../runnerFiles/PythonRunner.py\", line 1\n" +
+		"    print('Hi\n" +
+		"            ^\n" +
+		"SyntaxError: EOL while scanning string literal\n"
+
+	if err == nil {
+		t.Errorf("This should of failed and did not")
+	}
+
+	assertEquals(expected, actual, t)
+}
+
 func assertEquals(expected string, actual string, t *testing.T) {
 	if actual != expected {
+		i := 0
+		var expectedChar byte
+		var actualChar byte
+		for i < len(expected) && i < len(actual) {
+			if expected[i] != actual[i] {
+				expectedChar = expected[i]
+				actualChar = actual[i]
+				break
+			}
+			i++
+		}
 		t.Errorf("Expected \"%s\" but got \"%s\"", expected, actual)
+		t.Errorf("Error at index %d, expected %c but was %c", i, expectedChar, actualChar)
 	}
 }
