8dd4a3efe4c9f3b08ef86171b7b8ee61560d724c
Joshua Sterner <josh@joshuasterner.com>
2020-09-03 00:36:14 -0700

    Write server package.
    The server package exposes the code execution functionality of the
    executable package through JSON over HTTP. Currently, only one API
    endpoint, /execute/<language>, is implemented (with the exception of
    handling compilation and runtime errors in the executed code, as this
    depends on currently unimplemented work in the executable package).

---
+++ internal/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+    "context"
+    "fmt"
+    "net/http"
+)
+
+type HTTPServer interface {
+    Start(port int) error
+
+    Stop() error
+}
+
+func NewHTTPServer() HTTPServer {
+    return &httpServer{}
+}
+
+type httpServer struct {
+    server *http.Server
+}
+
+func (server *httpServer) Start(port int) error {
+    serveMux := newServeMux()
+    server.server = &http.Server {
+        Addr: fmt.Sprintf(":%d", port),
+        Handler: serveMux,
+    }
+    return server.server.ListenAndServe()
+}
+
+func (server *httpServer) Stop() error {
+    return server.server.Shutdown(context.Background())
+}
---
+++ internal/server/server_test.go
@@ -0,0 +1,29 @@
+package server
+
+import (
+    "testing"
+    "time"
+    "net/http"
+)
+
+func TestStartHTTPServer(t *testing.T) {
+    srv := NewHTTPServer()
+    go srv.Start(8765)
+    defer srv.Stop()
+    time.Sleep(1 * time.Second)
+    _, err := http.Get("http://localhost:8765")
+    if (err != nil) {
+        t.Errorf("Expected nil err from http.Get but got:  %s", err)
+    }
+}
+
+func TestStopHTTPServer(t *testing.T) {
+    srv := NewHTTPServer()
+    go srv.Start(8765)
+    time.Sleep(1 * time.Second)
+    srv.Stop()
+    _, err := http.Get("http://localhost:8765")
+    if (err == nil) {
+        t.Errorf("Expected connection refused error.")
+    }
+}
