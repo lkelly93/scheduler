8287eb5faf275c85ccc693572237f25b77b995fd
Luke Kelly <lkelly93@gmail.com>
2020-09-09 01:58:31 +0000

    More documentation

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/lkelly93/scheduler/internal/server"
 
+//This starts a scheduler server. The binary created from go build in this repo
+//will run a scheduler server but will not incase it in a container.
 func main() {
 	server := server.NewHTTPServer()
 	server.Start(3000)
